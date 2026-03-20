6a2d35d19cbdbfa275ca98a246f84dce3fe81e69
Isaac1805BC <isaacburgos543@gmail.com>
2026-03-19 19:34:20 -0500

    feat:Agregnado Disponibilidad y globalErrorHandler

---
+++ src/main/java/edu/dosw/proyect/exceptions/GlobalErrorHandler.java
@@ -0,0 +1,26 @@
+package edu.dosw.proyect.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalErrorHandler {
+
+    @ExceptionHandler(DisponibilidadException.class)
+    public ResponseEntity<Object> handleDisponibilidadException(DisponibilidadException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.CONFLICT.value());
+        body.put("error", "Conflicto de Regla de Negocio");
+        body.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.CONFLICT);
+    }
+
+}
