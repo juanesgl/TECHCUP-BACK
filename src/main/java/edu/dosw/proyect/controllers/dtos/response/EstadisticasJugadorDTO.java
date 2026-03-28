629ce510a22fa0bfe837d30c16bd3b990041cfe0
juanesgl <juan.sgarcia@mail.escuelaing.edu.co>
2026-03-28 13:16:57 -0500

    feat(statistics): implement core domain models and dtos

---
+++ src/main/java/edu/dosw/proyect/controllers/dtos/response/EstadisticasJugadorDTO.java
@@ -0,0 +1,19 @@
+package edu.dosw.proyect.controllers.dtos.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EstadisticasJugadorDTO {
+    private Long jugadorId;
+    private String nombreJugador;
+    private String nombreEquipo;
+    private int goles;
+    private int tarjetasAmarillas;
+    private int tarjetasRojas;
+}
---
+++ src/main/java/edu/dosw/proyect/core/models/enums/TipoEvento.java
@@ -0,0 +1,7 @@
+package edu.dosw.proyect.core.models.enums;
+
+public enum TipoEvento {
+    GOL,
+    TARJETA_AMARILLA,
+    TARJETA_ROJA
+}
