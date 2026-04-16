070b4819d9db993cd8bacfb4495d3f95d5dd29af
Natalia Mahecha <zharik.mahecha-c@mail.escuelaing.edu.co>
2026-04-16 10:48:10 -0500

    Se agrego url de versel a los origenes permitidos de cors

--- src/main/java/edu/dosw/proyect/config/CorsConfig.java
+++ src/main/java/edu/dosw/proyect/config/CorsConfig.java
@@ -13,6 +13,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("https://techcup-front.vercel.app");
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
         config.setAllowCredentials(true);
